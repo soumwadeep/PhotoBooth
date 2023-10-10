1d1b10cd6ae76dd7f245458f9b325e459e344ea3
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2023-10-10 11:19:48 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    color: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
